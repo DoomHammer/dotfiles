8fcbcceab64d99799bc738bb933aecc1bdeda315
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2024-07-27 20:29:39 +0100

    feat: Add yazi config

--- .config/nix/home-manager/features/yazi/default.nix
+++ .config/nix/home-manager/features/yazi/default.nix
@@ -1,7 +1,19 @@
 { config, flakePath, pkgs, ... }:
+
+let
+  flakePath = config: "${config.home.homeDirectory}/.config/nix";
+  # Out-of-store symlinks require absolute paths when using a flake config. This
+  # is because relative paths are expanded after the flake source is copied to
+  # a store path which would get us read-only store paths.
+  dir = "${flakePath config}/home-manager/features/yazi";
+in
 {
   home.packages = with pkgs; [
     yazi
+
+    unar # Handle archives
+    mediainfo # Display media information
+    exiftool # Read EXIF metadata
   ];
 
   programs.yazi = {
@@ -9,4 +21,7 @@
     enableBashIntegration = true;
     enableZshIntegration = true;
   };
+
+  xdg.configFile."yazi.toml".source =
+    config.lib.file.mkOutOfStoreSymlink "${dir}/yazi-config/yazi.toml";
 }
