f88e2ad46f1757bb852ca4569942622c8c530170
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2025-05-05 23:01:07 +0200

    feat(darwin): Use unstable UTM, remove XCode, add a few apps

--- .config/nix/systems/darwin/_mixins/desktop/apps/default.nix
+++ .config/nix/systems/darwin/_mixins/desktop/apps/default.nix
@@ -14,14 +14,23 @@
       audacity
       backblaze-downloader
       # TODO: Add Camtasia
+      # Potential inspiration:
+      # https://github.com/okpedersen/dotfiles/blob/fd9a42cdf1d9fa8a0e7ff6c30ec4309e19d90c43/spotify.nix#L32
+      # https://github.com/Shopify/nixpkgs/blob/9777a72dd3ff6f774351c264723862a13c2480ea/pkgs/by-name/zo/zoom-us/package.nix#L59
+      # https://github.com/noblepayne/lnd-boost-scraper/blob/4d91799daa9b3f4e5e2c1a0052603d427461d72f/module.nix#L38
+      # https://github.com/thefloweringash/nixpkgs/blob/ece4c62d4b3f6f53935ece6b9cd5efe644cee79b/pkgs/development/python-modules/gurobipy/darwin.nix#L6
+      # https://github.com/testfailed/nixos-config-srid/blob/df04659add20b7878e3c2a71e58c64399564bb26/overlays/shell-script/mosh.nix#L14
       # camtasia2019
       # cricut-design-space
+      darwin.lsusb
       fritzing
       gcc-arm-embedded-13
       gimp
       inkscape
+      jellyfin-media-player
       kitty
       lima
+      # netbird-ui
       openscad
       pngpaste
       rpiboot
@@ -44,9 +53,12 @@
     caskArgs.no_quarantine = true;
 
     # taps = builtins.attrNames config.nix-homebrew.taps;
-    taps = [ "netbirdio/tap" ];
+    taps = [
+      "netbirdio/tap"
+    ];
     brews = [
       "conan"
+      "sdl2"
     ];
     casks = [
       "ableton-live-standard"
@@ -54,6 +66,7 @@
       "autodesk-fusion"
       "balenaetcher"
       "backblaze"
+      "beeper"
       "creality-print"
       "focusrite-control"
       "freecad"
@@ -62,6 +75,7 @@
       "mqtt-explorer"
       "mu-editor"
       "musicbrainz-picard"
+      "netbird-ui"
       "nheko"
       "obs"
       "orcaslicer"
@@ -85,7 +99,7 @@
       "Linn Kazoo" = 848937349;
       "Numbers" = 409203825;
       "Pages" = 409201541;
-      "Xcode" = 497799835;
+      # "Xcode" = 497799835;
       "Zeroconf Browser" = 1355001318;
     };
   };
--- .config/nix/systems/darwin/_mixins/desktop/apps/utilities/default.nix
+++ .config/nix/systems/darwin/_mixins/desktop/apps/utilities/default.nix
@@ -2,7 +2,7 @@
 {
   environment.systemPackages = with pkgs; [
     grandperspective
-    utm
+    unstable.utm
   ];
 
   homebrew = {
