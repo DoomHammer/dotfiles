3cca653c8a91d7855b47b916c41942508ae2d3c0
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2026-08-17 14:36:28 +0200

    feat: Add precision config

--- .config/nix/systems/linux/precision/default.nix
+++ .config/nix/systems/linux/precision/default.nix
@@ -1,1 +1,18 @@
-_: { }
+{ pkgs, ... }: {
+  # Ubuntu 24.04
+
+  environment.systemPackages = with pkgs; [
+    lima
+  ];
+
+  # AppArmor profile for bubblewrap
+  environment.etc."apparmor.d/nix-bwrap".text = ''
+    abi <abi/4.0>,
+    include <tunables/global>
+
+    profile bwrap ${pkgs.bubblewrap}/bin/bwrap flags=(unconfined) {
+      userns,
+      include if exists <local/bwrap>
+    }
+  '';
+}
