e15367d893926257e7cbbbf07071ca29ae378ac7
Piotr Gaczkowski <DoomHammerNG@gmail.com>
2026-09-09 19:29:48 +0200

    fix: Quit deadnix moaning

--- .config/nix/home-manager/_mixins/features/glide-browser/default.nix
+++ .config/nix/home-manager/_mixins/features/glide-browser/default.nix
@@ -7,18 +7,16 @@
 let
   inherit (inputs.firefox-addons.lib.${pkgs.stdenv.hostPlatform.system}) buildFirefoxXpiAddon;
   firefox-addons = inputs.firefox-addons.packages.${pkgs.stdenv.hostPlatform.system};
-  mute-sites-by-default = (
-    buildFirefoxXpiAddon {
-      pname = "mute-sites-by-default";
-      version = "1.11resigned1";
-      addonId = "{@mute-sites-by-default}";
-      url = "https://addons.mozilla.org/firefox/downloads/file/4272832/mute_sites_by_default-1.11resigned1.xpi";
-      sha256 = "sha256-VPolBV4lyW+DxBLOvh9j/q9h3jfItKZjaJFu4R+mZO0=";
-      meta = with lib; {
-        platforms = platforms.all;
-      };
-    }
-  );
+  mute-sites-by-default = buildFirefoxXpiAddon {
+    pname = "mute-sites-by-default";
+    version = "1.11resigned1";
+    addonId = "{@mute-sites-by-default}";
+    url = "https://addons.mozilla.org/firefox/downloads/file/4272832/mute_sites_by_default-1.11resigned1.xpi";
+    sha256 = "sha256-VPolBV4lyW+DxBLOvh9j/q9h3jfItKZjaJFu4R+mZO0=";
+    meta = with lib; {
+      platforms = platforms.all;
+    };
+  };
 in
 {
   programs.glide-browser = {
